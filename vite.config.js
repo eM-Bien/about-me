ab23b2c4f142c5bf0c3351eb0b23ff5cbc411b23
Magda Bienkowska <m.lena.bienkowska@gmail.com>
2025-03-03 18:35:29 +0100

    change config file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/about-me/",
   plugins: [react()],
   build: {
-    outDir: "build",
+    outDir: "dist",
   },
 });
